c925f820e017db818b86ade3d794abb37c51c153
Marvin Engelbrecht <marvinengelbrecht30@gmail.com>
2024-04-30 09:36:57 +0200

    added get_input_from_options to main.py

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from Student import Student
 
+def get_input_from_options(options: list):
+    while True:
+        for index, option in enumerate(options):
+            print(f'{option}: {index + 1}')
+            
+        try:
+            user_input: int  = int(input('Input: '))
+        except ValueError:
+            continue
+        
+        if(user_input > len(options) or user_input <= 0 ):
+            continue
+        
+        return user_input
+        
+
 def main():
-    student = Student(0, 'marvin', 'engelbrecht')
-    print(student.grades)
-    student.add_grades_to_subject('math', [1,2,3])
-    student.add_grades_to_subject('math', [1,1,1])
-    print(student.grades)
+    user_input = get_input_from_options([
+        'option 1',
+        'second option',
+        'third option'
+    ])
+    
+    print(user_input)
     
 if __name__ == '__main__':
     main()
